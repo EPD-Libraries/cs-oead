b2cccfdd63a0288dbe0f8b8e90c450e695dea4cd
ArchLeaders <archleadership28@gmail.com>
2023-07-12 02:48:58 -0700

    Impl Resource helper

---
+++ tests/Helpers/Resource.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CsOead.Tests.Helpers;
+
+public static class Resource
+{
+    public static byte[] Input([CallerMemberName] string method = "", [CallerFilePath] string file = "")
+    {
+        return File.ReadAllBytes(
+            Path.Combine(
+                AppContext.BaseDirectory, "Data", Path.GetFileNameWithoutExtension(file), $"{method}.res")
+        );
+    }
+
+    public static string Output([CallerMemberName] string method = "", [CallerFilePath] string file = "")
+    {
+        return Path.Combine(
+            AppContext.BaseDirectory, "Data", Path.GetFileNameWithoutExtension(file), $"{method}.output.res");
+    }
+}
