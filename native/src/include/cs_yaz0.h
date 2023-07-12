33ea9476db274a7f53bc22961d9e36692598ba50
ArchLeaders <archleadership28@gmail.com>
2023-07-12 11:39:21 -0700

    Update oead include syntax

--- native/src/include/cs_yaz0.h
+++ native/src/include/cs_yaz0.h
@@ -1,5 +1,6 @@
+#include <oead/yaz0.h>
+
 #include "export.h"
-#include "oead/yaz0.h"
 
 EXP std::vector<u8>* Compress(u8* src, u32 src_len, u32 alignment, int level);
 EXP void Decompress(u8* src, u32 src_len, u8* dst, u32 dst_len);
