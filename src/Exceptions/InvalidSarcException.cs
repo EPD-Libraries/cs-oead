65438a0477758522e7d3b0423cf63a475d5e7abc
ArchLeaders <archleadership28@gmail.com>
2023-07-12 11:41:06 -0700

    Add InvalidSarcException

---
+++ src/Exceptions/InvalidSarcException.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace CsOead.Exceptions;
+
+public class InvalidSarcException : Exception
+{
+    public InvalidSarcException() { }
+    public InvalidSarcException(string? message) : base(message) { }
+    public InvalidSarcException(string? message, Exception? innerException) : base(message, innerException) { }
+    protected InvalidSarcException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
