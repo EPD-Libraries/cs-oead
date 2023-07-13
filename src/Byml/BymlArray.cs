08454f142ab06139b23f8ac6e48d1e732960b673
ArchLeaders <archleadership28@gmail.com>
2023-07-13 12:00:29 -0700

    Impl managed BymlArray

---
+++ src/Byml/BymlArray.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+
+namespace CsOead;
+
+public unsafe class BymlArray : SafeHandleZeroOrMinusOneIsInvalid, IEnumerable<Byml>, IEnumerable
+{
+    private bool _isChildNode = true;
+    private BymlArray() : base(true) { }
+
+    public BymlArray(bool _ = true) : base(true)
+    {
+        handle = BymlArrayNative.BymlArrayNew();
+        _isChildNode = false;
+    }
+
+    public static implicit operator BymlArray(Byml[] values) => new(values);
+
+    public BymlArray(params Byml[] values) : this((IEnumerable<Byml>)values) { }
+    public BymlArray(IEnumerable<Byml> values) : this(true)
+    {
+        foreach (var value in values) {
+            BymlArrayNative.BymlArrayAdd(this, value);
+        }
+    }
+
+    public int Length => BymlArrayNative.BymlArrayLength(this);
+
+    public Byml this[int index] {
+        get {
+            if (index >= Length) {
+                throw new IndexOutOfRangeException();
+            }
+
+            return BymlArrayNative.BymlArrayGet(this, index);
+        }
+        set => BymlArrayNative.BymlArraySet(this, index, value);
+    }
+
+    public void Add(Byml value)
+    {
+        BymlArrayNative.BymlArrayAdd(this, value);
+    }
+
+    public void Remove(Byml value)
+    {
+        BymlArrayNative.BymlArrayRemove(this, value);
+    }
+
+    public void RemoveAt(int index)
+    {
+        BymlArrayNative.BymlArrayRemoveAt(this, index);
+    }
+
+    public void Clear()
+    {
+        BymlArrayNative.BymlArrayClear(this);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerator<Byml> GetEnumerator()
+    {
+        return new Enumerator(this);
+    }
+
+    public struct Enumerator : IEnumerator<Byml>, IEnumerator
+    {
+        private readonly BymlArray _array;
+        private readonly int _length;
+        private int _index;
+
+        public Enumerator(BymlArray array)
+        {
+            _array = array;
+            _length = _array.Length;
+            _index = -1;
+        }
+
+        object IEnumerator.Current => Current;
+        public Byml Current {
+            get => BymlArrayNative.BymlArrayGet(_array, _index);
+        }
+
+        public bool MoveNext()
+        {
+            return ++_index < _length;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+
+        public void Dispose() { }
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        return _isChildNode || BymlArrayNative.BymlArrayFree(this);
+    }
+}
---
+++ src/Native/BymlArrayNative.cs
@@ -0,0 +1,32 @@
+﻿namespace CsOead.Native;
+
+internal static partial class BymlArrayNative
+{
+    [LibraryImport("cs_oead")]
+    internal static partial Byml BymlArrayGet(BymlArray array, int index);
+
+    [LibraryImport("cs_oead")]
+    internal static partial void BymlArraySet(BymlArray array, int index, Byml value);
+
+    [LibraryImport("cs_oead")]
+    internal static partial void BymlArrayAdd(BymlArray array, Byml value);
+
+    [LibraryImport("cs_oead")]
+    internal static partial void BymlArrayRemove(BymlArray array, Byml value);
+
+    [LibraryImport("cs_oead")]
+    internal static partial void BymlArrayRemoveAt(BymlArray array, int index);
+
+    [LibraryImport("cs_oead")]
+    internal static partial void BymlArrayClear(BymlArray array);
+
+    [LibraryImport("cs_oead")]
+    internal static partial int BymlArrayLength(BymlArray array);
+
+    [LibraryImport("cs_oead")]
+    internal static partial IntPtr BymlArrayNew();
+
+    [LibraryImport("cs_oead")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static partial bool BymlArrayFree(BymlArray array);
+}
