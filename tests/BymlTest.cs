202af0a8e8ee64ee51183eeceb5f27249719ba7b
ArchLeaders <archleadership28@gmail.com>
2023-07-13 10:55:37 -0700

    Update byml test

--- tests/BymlTest.cs
+++ tests/BymlTest.cs
@@ -1,6 +1,7 @@
 ﻿using CsOead.Exceptions;
 using CsOead.Native;
 using CsOead.Tests.Helpers;
+using System;
 using System.Text;
 
 namespace CsOead.Tests;
@@ -48,6 +49,59 @@ public class BymlTest
     {
         byte[] invalid = Resource.Input("Invalid");
         Byml.FromText(Encoding.UTF8.GetString(invalid));
+
+        return Task.CompletedTask;
+    }
+
+
+    [TestMethod]
+    public Task General()
+    {
+        byte[] raw = Resource.Input("FromBinary");
+        Byml byml = Byml.FromBinary(raw);
+
+        Assert.IsTrue(byml.Type == BymlType.Hash);
+
+        BymlHash root = byml.GetHash();
+
+        BymlHash bymlHash = root["byml_hash"].GetHash();
+        foreach ((var key, var value) in bymlHash) {
+            Assert.IsTrue(key switch {
+                "string" => value.Type == BymlType.String
+                    && value.GetString().ToString() == "A1B2C3",
+                "binary" => value.Type == BymlType.Binary
+                    && value.GetBinary().AsSpan().SequenceEqual("A1B2C3"u8),
+                "bool" => value.Type == BymlType.Bool
+                    && value.GetBool() == true,
+                "int" => value.Type == BymlType.Int
+                    && value.GetInt() == 0,
+                "uint" => value.Type == BymlType.UInt
+                    && value.GetUInt() == 0x0U,
+                "float" => value.Type == BymlType.Float
+                    && value.GetFloat() == 0.0,
+                "int64" => value.Type == BymlType.Int64
+                    && value.GetInt64() == 0x0L,
+                "uint64" => value.Type == BymlType.UInt64
+                    && value.GetUInt64() == 0x0UL,
+                "double" => value.Type == BymlType.Double
+                    && value.GetDouble() == 0.0D,
+                _ => false
+            });
+        }
+
+        bymlHash.Remove("string");
+
+        Assert.IsFalse(bymlHash.TryGet("string", out _));
+        Assert.IsFalse(bymlHash.ContainsKey("string"));
+        Assert.ThrowsException<KeyNotFoundException>(() => bymlHash["string"]);
+
+        bymlHash["string"] = "A1B2C3";
+        Assert.IsTrue(bymlHash["string"].GetString().ToString() == "A1B2C3");
+
+        bymlHash.Clear();
+        Assert.IsFalse(bymlHash.Count > 0);
+
+        BymlHash bymlArray = root["byml_hash"].GetHash();
 
         return Task.CompletedTask;
     }
