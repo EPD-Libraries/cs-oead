b9724962ee3fc0667b39166aeb82bfaff41bfce7
ArchLeaders <archleadership28@gmail.com>
2023-07-12 21:43:34 -0700

    Add byml tests

---
+++ tests/BymlTest.cs
@@ -0,0 +1,81 @@
+﻿using CsOead.Exceptions;
+using CsOead.Native;
+using CsOead.Tests.Helpers;
+using System.Text;
+
+namespace CsOead.Tests;
+
+[TestClass]
+public class BymlTest
+{
+    static BymlTest() => OeadContext.Init();
+
+    [TestMethod]
+    public Task FromBinary()
+    {
+        byte[] raw = Resource.Input();
+        Byml byml = Byml.FromBinary(raw);
+
+        Assert.IsTrue(byml.Type == BymlType.Hash);
+
+        return Task.CompletedTask;
+    }
+
+    [TestMethod]
+    public Task FromText()
+    {
+        byte[] raw = Resource.Input();
+        Byml byml = Byml.FromText(Encoding.UTF8.GetString(raw));
+
+        Assert.IsTrue(byml.Type == BymlType.Hash);
+
+        return Task.CompletedTask;
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidBymlException))]
+    public Task InvalidFromBinary()
+    {
+        byte[] invalid = Resource.Input("Invalid");
+        Byml.FromBinary(invalid);
+
+        return Task.CompletedTask;
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidBymlException))]
+    public Task InvalidFromText()
+    {
+        byte[] invalid = Resource.Input("Invalid");
+        Byml.FromText(Encoding.UTF8.GetString(invalid));
+
+        return Task.CompletedTask;
+    }
+
+    [TestMethod]
+    public Task ToBinary()
+    {
+        byte[] raw = Resource.Input("FromBinary");
+        Span<byte> buffer = Byml.FromBinary(raw).ToBinary();
+
+        Byml byml = Byml.FromBinary(buffer);
+
+        Assert.IsTrue(byml.Type == BymlType.Hash);
+
+        return Task.CompletedTask;
+    }
+
+    [TestMethod]
+    public Task ToText()
+    {
+        byte[] raw = Resource.Input("FromBinary");
+        string text = Byml.FromBinary(raw).ToText().ToString();
+
+
+        Byml byml = Byml.FromText(text);
+
+        Assert.IsTrue(byml.Type == BymlType.Hash);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/Data/BymlTest/FromText.res
@@ -0,0 +1,16 @@
+byml_array:
+  - 0
+  - 1.0
+  - !u 0x02
+  - !l 3
+  - "4"
+byml_hash:
+  - string: "A1B2C3"
+    binary: !!binary QTFCMkMz
+    bool: true
+    int: 0
+    uint: !u 0x00000000
+    float: 0.0
+    int64: !l 0
+    uint64: !ul 0x00000000
+    double: !f64 0.0
