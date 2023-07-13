5746e190da8071173171b7226b1ea33e3888b050
ArchLeaders <archleadership28@gmail.com>
2023-07-12 18:55:19 -0700

    Document Sarc wrapper

--- src/Sarc.cs
+++ src/Sarc.cs
@@ -14,11 +14,16 @@ public enum Mode
     Legacy, New
 }
 
+/// <summary>
+/// Instance wrapper around the <a href="https://oead.readthedocs.io/en/stable/sarc.html"><c>oead::Sarc</c></a> class<br/>
+/// <b>Note:</b> This API does not match the c++ documentation entirely, the SarcWriter is now embedded into the <see cref="Sarc"/> wrapper
+/// </summary>
 public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair<string, DataMarshal>>, IEnumerable
 {
+    [Obsolete("Please use the Sarc(Endianness, Mode) constructor to create a new empty archive")]
     public Sarc() : base(true) { }
 
-    public Sarc(Endianness endian = Endianness.Little, Mode mode = Mode.New) : base(true)
+    public Sarc(Endianness endian, Mode mode = Mode.New) : base(true)
     {
         _writer = SarcNative.SarcWriterNew(endian, mode);
     }
@@ -30,12 +35,22 @@ public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair
 
     private bool IsWriter => _writer > -1;
 
+    /// <summary>
+    /// Gets or sets the file matching the provided <paramref name="key"/>
+    /// </summary>
+    /// <exception cref="KeyNotFoundException"/>
     public ReadOnlySpan<byte> this[string key] {
         get => GetFile(key);
         set => Add(key, value);
     }
 
+    /// <summary>
+    /// Loads the input file or buffer into an unmanaged <a href="https://oead.readthedocs.io/en/stable/sarc.html#_CPPv4N4oead4SarcE">
+    /// <c>oead::Sarc*</c></a> instance and returns the managed wrapper
+    /// </summary>
     public static Sarc FromBinary(string inputFile) => FromBinary(File.ReadAllBytes(inputFile));
+
+    /// <inheritdoc cref="FromBinary(string)"/>
     public static Sarc FromBinary(ReadOnlySpan<byte> data)
     {
         fixed (byte* ptr = data) {
@@ -44,7 +59,14 @@ public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair
         }
     }
 
+    /// <summary>
+    /// Writes the underlying <a href="https://oead.readthedocs.io/en/stable/sarc.html#_CPPv4N4oead10SarcWriterE">
+    /// <c>oead::SarcWriter*</c></a> and returns a <see cref="DataMarshal"/> of the
+    /// unmanaged <a href="https://en.cppreference.com/w/cpp/container/vector"><c>std::vector&lt;u8&gt;</c></a>
+    /// </summary>
     public DataMarshal ToBinary(Endianness? endianness = null, Mode? mode = null) => ToBinary(out _, endianness, mode);
+
+    /// <inheritdoc cref="ToBinary(Endianness?, Mode?)"/>
     public DataMarshal ToBinary(out uint alignment, Endianness? endianness = null, Mode? mode = null)
     {
         if (endianness is Endianness _endianness) {
@@ -68,6 +90,12 @@ public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair
         set => SarcNative.SarcWriterSetEndianness(Writer, value);
     }
 
+    /// <summary>
+    /// Returns a span of data matching the provided <paramref name="key"/><br/>
+    /// <b>Note:</b> the life of the returned <see cref="Span{T}"/> lasts only
+    /// as long as the parent <see cref="Sarc"/>
+    /// </summary>
+    /// <exception cref="KeyNotFoundException"></exception>
     public Span<byte> GetFile(string key)
     {
         if (TryGetFile(key, out Span<byte> value)) {
@@ -78,6 +106,13 @@ public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair
             $"The key '{key}' was not found in the archive");
     }
 
+    /// <summary>
+    /// Looks for the provided <paramref name="key"/> and sets the output
+    /// <paramref name="value"/> if it exists in the archive (otherwise <see langword="null"/>)
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    /// <returns><see langword="true"/> if the key exists in the archive, otherwise <see langword="false"/></returns>
     public bool TryGetFile(string key, out Span<byte> value)
     {
         bool result = IsWriter ? SarcNative.SarcWriterGetFile(Writer, key, out byte* dst, out int dst_len)
@@ -87,11 +122,19 @@ public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair
         return result;
     }
 
+    /// <summary>
+    /// Checks if the provided <paramref name="key"/> exists in the archive
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     public bool ContainsKey(string key)
     {
         return SarcNative.SarcWriterContainsKey(Writer, key);
     }
 
+    /// <summary>
+    /// Adds a new file to the archive or sets the value if it already exists
+    /// </summary>
     public void Add(string key, ReadOnlySpan<byte> value)
     {
         fixed (byte* ptr = value) {
@@ -99,11 +142,17 @@ public unsafe class Sarc : SafeHandleMinusOneIsInvalid, IEnumerable<KeyValuePair
         }
     }
 
+    /// <summary>
+    /// Removes the provided <paramref name="key"/> from the archive
+    /// </summary>
     public void Remove(string key)
     {
         SarcNative.SarcWriterRemoveFile(Writer, key);
     }
 
+    /// <summary>
+    /// Removes every file from the archive
+    /// </summary>
     public void Clear()
     {
         SarcNative.SarcWriterClearFiles(Writer);
